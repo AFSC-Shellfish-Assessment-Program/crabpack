9a6dc650c44d63f1b982ae4e671a73e692407e31
Shannon Hennessey <s.hennessey13@gmail.com>
2024-11-08 10:38:13 -0900

    add package-level import documentation

---
+++ R/crabPack-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import data.table
+#' @import tidyverse
+#' @importFrom dplyr mutate
+#' @importFrom dplyr case_when
+#' @importFrom dplyr filter
+#' @importFrom stats aggregate
+#' @importFrom stats weighted.mean
+## usethis namespace: end
+NULL
