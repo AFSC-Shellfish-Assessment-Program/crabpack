2343e9f5fc2765903c1eecf7e79dcd47e2c39943
Shannon Hennessey <s.hennessey13@gmail.com>
2024-11-29 10:11:43 -0900

    fix typos in documentation

--- R/get_male_ogives.R
+++ R/get_male_ogives.R
@@ -1,19 +1,19 @@
 
 #' @description .... time series of Tanner Crab and Snow Crab male ogives...proportion mature
-#'              Also yearly parmeter estimates....
+#'              Also yearly parameter estimates....
 #'
 #' @param species character string. One of c("Tanner Crab", "Snow Crab").
 #'                c("TANNER", "SNOW").
 #' @param district character string. One of c("ALL", "E166", "W166"). Defaults to
 #'                 "ALL", "E166" and "W166" are to be used for Tanner Crab only.
 #' @param output character string. One of c("estimates", "parameters"). Defaults
-#'               to "estimates" to provide ___. Specift "parameters" for a table
+#'               to "estimates" to provide ___. Specify "parameters" for a table
 #'               of model parameter estimates for each year.
 #' @inheritParams get_specimen_data
 #'
 #' @return
 #'
-#' @export get_specimen_data
+#' @export
 #'
 
 get_male_ogives <- function(species = NULL,
