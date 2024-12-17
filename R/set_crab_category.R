45420d8c9f3deb76e9f2fe6581bf19f59d705a8c
Shannon Hennessey <s.hennessey13@gmail.com>
2024-12-17 09:16:53 -0900

    rm 0-catch stations from specimen data, join size min/max and then assign category --> speeds code up a lot

--- R/set_crab_category.R
+++ R/set_crab_category.R
@@ -35,41 +35,58 @@ set_crab_category <- function(crab_data = NULL,
   ## Get crab category size definitions
   sizegroups <- crab_data$sizegroups
 
+  # Pull specimen dataframe, remove 0-catch stations
+  specimen_dat <- crab_data$specimen %>%
+                  dplyr::filter(!is.na(SEX))
+
 
   ## Define 'maturity district' to accommodate unstratified BKC
   # BKC below 58.65 deg lat --> PRIB; BKC above 58.65 deg lat --> STMATT
   # all other species, maturity tied to defined districts
-  crab_data <- crab_data %>%
-               dplyr::mutate(MAT_DISTRICT = DISTRICT,
-                             MAT_DISTRICT = dplyr::case_when((DISTRICT == "UNSTRAT" & MID_LATITUDE < 58.65) ~ "PRIB",
-                                                             (DISTRICT == "UNSTRAT" & MID_LATITUDE >= 58.65) ~ "STMATT",
-                                                             TRUE ~ MAT_DISTRICT))
+  specimen_dat <- specimen_dat %>%
+                  dplyr::mutate(MAT_DISTRICT = DISTRICT,
+                                MAT_DISTRICT = dplyr::case_when((DISTRICT == "UNSTRAT" & LATITUDE < 58.65) ~ "PRIB",
+                                                                (DISTRICT == "UNSTRAT" & LATITUDE >= 58.65) ~ "STMATT",
+                                                                TRUE ~ MAT_DISTRICT))
 
 
   ## Assign female maturity -- default to "morphometric" ----------------------
   if(female_maturity == "cutline"){
     # look up cutlines for mature and immature females
     if(!species == "HAIR"){
-      imm_fem <- sizegroups %>%
-                 dplyr::filter(SEX == 2,
-                               CATEGORY == "immature_female")
-      mat_fem <- sizegroups %>%
-                 dplyr::filter(SEX == 2,
-                               CATEGORY == "mature_female")
 
-      female_dat <- crab_data %>%
-                    dplyr::filter(SEX == 2) %>%
-                    dplyr::mutate(CATEGORY = case_when((SIZE >= mat_fem$SIZE_MIN[mat_fem$DISTRICT == MAT_DISTRICT]) ~ "mature_female",
-                                                       (SIZE <= imm_fem$SIZE_MAX[imm_fem$DISTRICT == MAT_DISTRICT]) ~ "immature_female"))
+      fem_dat <- specimen_dat %>%
+                 dplyr::filter(SEX == 2) %>%
+                 left_join(., sizegroups %>%
+                              dplyr::filter(SEX == 2) %>%
+                              dplyr::rename(MAT_DISTRICT = DISTRICT),
+                           relationship = "many-to-many") %>%
+                 dplyr::mutate(RANGE = ifelse(SIZE_1MM >= SIZE_MIN & SIZE_1MM <= SIZE_MAX, 1, NA)) %>%
+                 dplyr::filter(!is.na(RANGE)) %>%
+                 dplyr::select(-c('RANGE', 'SIZE_MIN', 'SIZE_MAX'))
+
+      # imm_fem <- sizegroups %>%
+      #            dplyr::filter(SEX == 2,
+      #                          CATEGORY == "immature_female")
+      # mat_fem <- sizegroups %>%
+      #            dplyr::filter(SEX == 2,
+      #                          CATEGORY == "mature_female")
+      #
+      # female_dat <- specimen_dat %>%
+      #               dplyr::filter(SEX == 2) %>%
+      #               dplyr::rowwise() %>%
+      #               dplyr::mutate(CATEGORY = case_when((SIZE_1MM >= mat_fem$SIZE_MIN[mat_fem$DISTRICT == MAT_DISTRICT]) ~ "mature_female",
+      #                                                  (SIZE_1MM <= imm_fem$SIZE_MAX[imm_fem$DISTRICT == MAT_DISTRICT]) ~ "immature_female",
+      #                                                  TRUE ~ NA))
       } else{
         # no female maturity cutline for hair crab, just "female"
-        female_dat <- crab_data %>%
+        female_dat <- specimen_dat %>%
                       dplyr::filter(SEX == 2) %>%
                       dplyr::mutate(CATEGORY = "female")
       }
   } else{
     # define female maturity by clutch size
-    female_dat <- crab_data %>%
+    female_dat <- specimen_dat %>%
                   dplyr::filter(SEX == 2) %>%
                   dplyr::mutate(CATEGORY = case_when(CLUTCH_SIZE >= 1 ~ "mature_female",
                                                      CLUTCH_SIZE == 0 ~ "immature_female"))
@@ -79,89 +96,114 @@ set_crab_category <- function(crab_data = NULL,
   ## Assign male maturity, and legal/industry-preferred sizes if applicable ----
   # Assign immature, mature, legal for King Crab spp.
   if(species %in% c("RKC", "BKC")){
-    # mature/immature males
-    imm_male <- sizegroups %>%
-                dplyr::filter(SEX == 1,
-                              CATEGORY == "immature_male")
-    mat_male <- sizegroups %>%
-                dplyr::filter(SEX == 1,
-                              CATEGORY == "mature_male")
+    # immature, mature, legal males
+    male_dat <- specimen_dat %>%
+                dplyr::filter(SEX == 1) %>%
+                left_join(., sizegroups %>%
+                             dplyr::filter(SEX == 1) %>%
+                             dplyr::rename(MAT_DISTRICT = DISTRICT),
+                          relationship = "many-to-many") %>%
+                dplyr::mutate(RANGE = ifelse(SIZE_1MM >= SIZE_MIN & SIZE_1MM <= SIZE_MAX, 1, NA)) %>%
+                dplyr::filter(!is.na(RANGE)) %>%
+                dplyr::select(-c('RANGE', 'SIZE_MIN', 'SIZE_MAX'))
 
-    mat_male_dat <- crab_data %>%
-                    dplyr::filter(SEX == 1) %>%
-                    dplyr::mutate(CATEGORY = case_when((SIZE >= mat_male$SIZE_MIN[mat_male$DISTRICT == MAT_DISTRICT]) ~ "mature_male",
-                                                       (SIZE <= imm_male$SIZE_MAX[imm_male$DISTRICT == MAT_DISTRICT]) ~ "immature_male"))
-
-    # legal males
-    leg_male <- sizegroups %>%
-                dplyr::filter(SEX == 1,
-                              CATEGORY == "legal_male")
-
-    leg_male_dat <- crab_data %>%
-                    dplyr::filter(SEX == 1) %>%
-                    dplyr::mutate(CATEGORY = case_when((SIZE >= leg_male$SIZE_MIN[leg_male$DISTRICT == MAT_DISTRICT]) ~ "legal_male"))
-
-    male_dat <- rbind(mat_male_dat, leg_male_dat)
+    # imm_male <- sizegroups %>%
+    #             dplyr::filter(SEX == 1,
+    #                           CATEGORY == "immature_male")
+    # mat_male <- sizegroups %>%
+    #             dplyr::filter(SEX == 1,
+    #                           CATEGORY == "mature_male")
+    #
+    # mat_male_dat <- specimen_dat %>%
+    #                 dplyr::filter(SEX == 1) %>%
+    #                 dplyr::rowwise() %>%
+    #                 dplyr::mutate(CATEGORY = case_when((SIZE_1MM >= mat_male$SIZE_MIN[mat_male$DISTRICT == MAT_DISTRICT]) ~ "mature_male",
+    #                                                    (SIZE_1MM <= imm_male$SIZE_MAX[imm_male$DISTRICT == MAT_DISTRICT]) ~ "immature_male"))
+    #
+    # # legal males
+    # leg_male <- sizegroups %>%
+    #             dplyr::filter(SEX == 1,
+    #                           CATEGORY == "legal_male")
+    #
+    # leg_male_dat <- specimen_dat %>%
+    #                 dplyr::filter(SEX == 1) %>%
+    #                 dplyr::rowwise() %>%
+    #                 dplyr::mutate(CATEGORY = case_when((SIZE_1MM >= leg_male$SIZE_MIN[leg_male$DISTRICT == MAT_DISTRICT]) ~ "legal_male")) %>%
+    #                 dplyr::filter(!is.na(CATEGORY))
+    #
+    # male_dat <- rbind(mat_male_dat, leg_male_dat)
   }
 
   # Assign small, large, legal, preferred for Chionoecetes spp.
   if(species %in% c("SNOW", "TANNER", "HYBRID")){
-    # large/small males
-    small_male <- sizegroups %>%
-                  dplyr::filter(SEX == 1,
-                                CATEGORY == "small_male")
-    large_male <- sizegroups %>%
-                  dplyr::filter(SEX == 1,
-                                CATEGORY == "large_male")
+    # small, large, legal, preferred males
+    male_dat <- specimen_dat %>%
+                dplyr::filter(SEX == 1) %>%
+                left_join(., sizegroups %>%
+                            dplyr::filter(SEX == 1) %>%
+                            dplyr::rename(MAT_DISTRICT = DISTRICT),
+                          relationship = "many-to-many") %>%
+                dplyr::mutate(RANGE = ifelse(SIZE_1MM >= SIZE_MIN & SIZE_1MM <= SIZE_MAX, 1, NA)) %>%
+                dplyr::filter(!is.na(RANGE)) %>%
+                dplyr::select(-c('RANGE', 'SIZE_MIN', 'SIZE_MAX'))
 
-    mat_male_dat <- crab_data %>%
-                    dplyr::filter(SEX == 1) %>%
-                    dplyr::mutate(CATEGORY = case_when((SIZE >= large_male$SIZE_MIN[large_male$DISTRICT == MAT_DISTRICT]) ~ "large_male",
-                                                       (SIZE <= small_male$SIZE_MAX[small_male$DISTRICT == MAT_DISTRICT]) ~ "small_male"))
-
-    # legal males
-    leg_male <- sizegroups %>%
-                dplyr::filter(SEX == 1,
-                              CATEGORY == "legal_male")
-
-    leg_male_dat <- crab_data %>%
-                    dplyr::filter(SEX == 1) %>%
-                    dplyr::mutate(CATEGORY = case_when((SIZE >= leg_male$SIZE_MIN[leg_male$DISTRICT == MAT_DISTRICT]) ~ "legal_male"))
-
-
-    # industry-preferred males
-    pref_male <- sizegroups %>%
-                 dplyr::filter(SEX == 1,
-                               CATEGORY == "preferred_male")
-
-    ind_pref_dat <- crab_data %>%
-                    dplyr::filter(SEX == 1) %>%
-                    dplyr::mutate(CATEGORY = case_when((SIZE >= pref_male$SIZE_MIN[pref_male$DISTRICT == MAT_DISTRICT]) ~ "preferred_male"))
-
-
-    male_dat <- rbind(mat_male_dat, leg_male_dat, ind_pref_dat)
+    # small_male <- sizegroups %>%
+    #               dplyr::filter(SEX == 1,
+    #                             CATEGORY == "small_male")
+    # large_male <- sizegroups %>%
+    #               dplyr::filter(SEX == 1,
+    #                             CATEGORY == "large_male")
+    #
+    # mat_male_dat <- specimen_dat %>%
+    #                 dplyr::filter(SEX == 1) %>%
+    #                 dplyr::rowwise() %>%
+    #                 dplyr::mutate(CATEGORY = case_when((SIZE_1MM >= large_male$SIZE_MIN[large_male$DISTRICT == MAT_DISTRICT]) ~ "large_male",
+    #                                                    (SIZE_1MM <= small_male$SIZE_MAX[small_male$DISTRICT == MAT_DISTRICT]) ~ "small_male"))
+    #
+    # # legal males
+    # leg_male <- sizegroups %>%
+    #             dplyr::filter(SEX == 1,
+    #                           CATEGORY == "legal_male")
+    #
+    # leg_male_dat <- specimen_dat %>%
+    #                 dplyr::filter(SEX == 1) %>%
+    #                 dplyr::rowwise() %>%
+    #                 dplyr::mutate(CATEGORY = case_when((SIZE_1MM >= leg_male$SIZE_MIN[leg_male$DISTRICT == MAT_DISTRICT]) ~ "legal_male")) %>%
+    #                 dplyr::filter(!is.na(CATEGORY))
+    #
+    #
+    # # industry-preferred males
+    # pref_male <- sizegroups %>%
+    #              dplyr::filter(SEX == 1,
+    #                            CATEGORY == "preferred_male")
+    #
+    # ind_pref_dat <- specimen_dat %>%
+    #                 dplyr::filter(SEX == 1) %>%
+    #                 dplyr::rowwise() %>%
+    #                 dplyr::mutate(CATEGORY = case_when((SIZE_1MM >= pref_male$SIZE_MIN[pref_male$DISTRICT == MAT_DISTRICT]) ~ "preferred_male")) %>%
+    #                 dplyr::filter(!is.na(CATEGORY))
+    #
+    #
+    # male_dat <- rbind(mat_male_dat, leg_male_dat, ind_pref_dat)
   }
 
   # Assign sublegal, legal for Hair Crab
   if(species == "HAIR"){
-    # legal/sublegal males
-    sub_male <- sizegroups %>%
-                dplyr::filter(SEX == 1,
-                              CATEGORY == "sublegal_male")
-    leg_male <- sizegroups %>%
-                dplyr::filter(SEX == 1,
-                              CATEGORY == "legal_male")
-
-    male_dat <- crab_data %>%
+    # legal, sublegal males
+    male_dat <- specimen_dat %>%
                 dplyr::filter(SEX == 1) %>%
-                dplyr::mutate(CATEGORY = case_when((SIZE >= leg_male$SIZE_MIN[leg_male$DISTRICT == MAT_DISTRICT]) ~ "legal_male",
-                                                   (SIZE <= sub_male$SIZE_MAX[sub_male$DISTRICT == MAT_DISTRICT]) ~ "sublegal_male"))
-  }
+                left_join(., sizegroups %>%
+                            dplyr::filter(SEX == 1) %>%
+                            dplyr::rename(MAT_DISTRICT = DISTRICT),
+                          relationship = "many-to-many") %>%
+                dplyr::mutate(RANGE = ifelse(SIZE_1MM >= SIZE_MIN & SIZE_1MM <= SIZE_MAX, 1, NA)) %>%
+                dplyr::filter(!is.na(RANGE)) %>%
+                dplyr::select(-c('RANGE', 'SIZE_MIN', 'SIZE_MAX'))
+    }
 
 
   ## Bind male and female data
   category_dat <- rbind(male_dat, female_dat) %>%
-                  dplyr::filter(!is.na(CATEGORY)) %>%
                   dplyr::select(-MAT_DISTRICT)
 
   return(category_dat)
